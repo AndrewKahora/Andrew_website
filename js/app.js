7717e2c2e3c7595ff13480790178bf025f63722e
AndrewKahora <cahndrew@gmail.com>
2016-10-15 15:39:57 +0300

    progress bars

--- js/app.js
+++ js/app.js
@@ -15,3 +15,16 @@ function sticky_relocate() {
       $(window).scroll(sticky_relocate);
       sticky_relocate();
     });
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
